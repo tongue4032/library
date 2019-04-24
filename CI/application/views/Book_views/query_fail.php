e82f833656e4020d82495316f57a1f20c5728b6e
tongue <tongue4032@outlook.com>
2019-04-24 19:18:46 +0800

    js修改

--- CI/application/views/Book_views/query_fail.php
+++ CI/application/views/Book_views/query_fail.php
@@ -2,55 +2,51 @@
 <html lang="en">
 
 <head>
-    <title>ONE Library</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="utf-8">
-    <!-- <meta name="keywords" content="Border sign in Form a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design"> -->
+    <title>Sleet a Flat Responsive Widget Template :: w3layouts</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Sleet Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design"
+    />
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
 
-    <link rel="stylesheet" href="<?='/css/style7.css'?>" type="text/css" media="all">
-    <link rel="stylesheet" href="<?='/css/font-awesome.css'?>" type="text/css" media="all">
-    <link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=devanagari,latin-ext" rel="stylesheet">
-</head>
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- font files -->
+    <link href="//fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Rancho" rel="stylesheet">
+    <!-- /font files -->
+    <!-- css files -->
+    <link href="<?='/css/error.css'?>" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?='/css/font-awesome.css'?>" rel="stylesheet" type="text/css" media="all" />
+
+    <!-- /css files -->
 
 <body>
-<h1 class="title-agile text-center">ONE Library</h1>
-<div class="content-w3ls">
-    <a style="color: #fff" href="<?='/Secure'?>"><span class="fa fa-reply" aria-hidden="true">BACK</span></a>
-    <form>
 
-    </form>
+<div class="container-w3layouts  text-center">
+    <div class="agileits-logo">
+        <h1>
+            <a href="<?='/'?>">
+                <span class="fa fa-spinner" aria-hidden="true"></span>ONELibrary</a>
+        </h1>
+    </div>
+    <h2 class="txt-wthree">error 404</h2>
+    <p>Looks like the page you are trying to visit does not exist
+        <br> Please check the URL and Try again.</p>
+    <div class="home">
+        <a href="<?='/'?>">home</a>
+    </div>
 </div>
-
-<div class="text-center copyright" style="width: 100%;position:absolute;bottom:10px;left:0px;">
-    <p>© 2019 ONE Book lending. All rights reserved | Design by Tony</p>
+<div class="w3_agile-footer">
+    <p> &copy; 2017 Sleet. All Rights Reserved | Design by
+        <a href="http://w3layouts.com" target="=_blank">W3layouts</a>
+    </p>
 </div>
-<script src="/js/jquery-2.1.4.min.js"></script>
-<script>
-    addEventListener("load", function () {
-        setTimeout(hideURLbar, 0);
-    }, false);
-
-    function hideURLbar() {
-        window.scrollTo(0, 1);
-    }
-
-    // function encode(){
-    //     $.ajax( {
-    //         type : "GET",
-    //         url:"/Book/borrow",
-    //         dataType:"json",
-    //         data:data,
-    //         success : function(data) {
-    //             if (data.code == 1) {
-    //                 alert(data.message);
-    //                 window.location='/Book/info';
-    //             }else{
-    //                 alert(data.message);
-    //             }
-    //         }
-    //     });
-    // }
-</script>
 </body>
-<!-- //Body -->
+
 </html>
